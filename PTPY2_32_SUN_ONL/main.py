a10fdffea8a6ac73c57c0f1d47c58c90f6efd4f3
KhanhDuyDang91 <khanhduydang910@gmail.com>
2025-01-12 12:10:46 +0700

    lesson2

--- PTPY2_32_SUN_ONL/main.py
+++ PTPY2_32_SUN_ONL/main.py
@@ -22,3 +22,42 @@ def check_int(str_num):
         return int(str_num)
     except:
         return False
+
+class InstrScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        instr = Label(text = txt_instruction)
+        lbl1 = Label(text = 'Enter the name:', halign = 'right')
+        self.in_name = TextInput(multiline = False)
+        lbl2 = Label(text='Enter the age:', halign = 'right')
+        self.in_age = TextInput(text='7', multiline = False)
+        self.btn = Button(text='Start', size_hint=(0.3, 0.2), pos_hint={'center_x':0.5})
+        self.btn.on_press = self.next
+
+        line1 = BoxLayout(size_hint=(0.8, None), height='30sp')
+        line2 = BoxLayout(size_hint=(0.8, None), height='30sp')
+        line1.add_widget(lbl1)
+        line1.add_widget(self.in_name)
+        line2.add_widget(lbl2)
+        line2.add_widget(self.in_age)
+
+        outer = BoxLayout(orientation ='vertical', padding = 8, spacing = 8)
+        outer.add_widget(instr)
+        outer.add_widget(line1)
+        outer.add_widget(line2)
+        outer.add_widget(self.btn)
+        self.add_widget(outer)
+
+    def next(self):
+        global name
+        name = self.in_name.text
+        self.manager.current = 'purse1'
+
+class HeartCheck(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(InstrScreen(name = 'instr'))
+        return sm
+    
+app = HeartCheck()
+app.run()
